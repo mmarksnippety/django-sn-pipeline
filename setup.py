81f38b2f73b57466f83cbbf06bf898c2955f58b5
Marcin Markiewicz <marcin.m@webinterpret.com>
2015-09-23 10:31:50 +0200

    Change name from sn-django-* to django-sn-*

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='sn-django-pipeline',
+    name='django-sn-pipeline',
     version='0.1',
     description='Django Pipeline Sassc compiler',
     # url='http://github.com/storborg/funniest',
     author='Marcin Markiewicz',
     author_email='marcin@snippety.pl',
     license='MIT',
-    packages=find_packages(include=('sn-django-pipeline*', )),
+    packages=find_packages(include=('django-sn-pipeline*', )),
     install_requires=open('requirements.txt').read(),
     zip_safe=False
 )
